3b366c12f03c59948434c5541ad19b96f24c74f2
adevlinb <devlinbgenassembly@gmail.com>
2022-02-04 17:26:51 -0800

    adds dotenv

--- finchcollector/settings.py
+++ finchcollector/settings.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+
+DJANGO_SECRET_KEY = str(os.getenv('DJANGO_SECRET_KEY'))
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q&-=rgn%#3f4ycobbn_k1*xo4rg733s2j-!64x_0qmubn&kd_j'
+SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
